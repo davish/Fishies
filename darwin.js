4d0b560a316cb9df1e52be4e13d4c24074036e7d
Davis Haupt <dbh937@gmail.com>
2015-07-03 14:18:23 -0700

    updated syntax for es6

--- darwin.js
+++ darwin.js
@@ -18,50 +18,54 @@
 	survivalRate is an int (smaller than popSize) that represents how many organisms of the previous generation moves on to the next generation.
 	tournament is a boolean value of whether the successors(population) function determines the successors based on tournament or roulette wheel.
  */
-function Darwin(fitness, mate, mutate, generateOrganism, simulate, popSize, survivalRate, tournament) {
-	// create the first generation
-	this.fitness = fitness;
-	this.mate = mate;
-	this.mutate = mutate;
-	this.simulate = simulate;
-	this.popSize = popSize;
-	this.survivalRate = survivalRate < popSize ? survivalRate : popSize;
-	
-	this.population = [];
+ 
+class Darwin { 
+	constructor(fitness, mate, mutate, generateOrganism, simulate, popSize, survivalRate, tournament) {
+		// create the first generation
+		this.fitness = fitness;
+		this.mate = mate;
+		this.mutate = mutate;
+		this.simulate = simulate;
+		this.popSize = popSize;
+		this.survivalRate = survivalRate < popSize ? survivalRate : popSize;
+		
+		this.population = [];
 
-	// gotta generate the first generation
-	for (var i=0; i < popSize; i++) {
-		this.population.push(generateOrganism());
-	}
-}
-Darwin.prototype.step = function(roundNum) {
-	this.simulate(this.population, roundNum);
-	var parents = this.successors(this.population);
-	var nextGen = [];
-	while (nextGen.length < this.popSize) {
-		// create two new organisms from two random parents.
-		var nemo = this.mate(parents[Math.floor(Math.random() * parents.length)], parents[Math.floor(Math.random() * parents.length)]);
-		if (typeof nemo.indexOf === "undefined")
-			this.mutate(nemo);
-		else {
-			for (var j=0; j < nemo.length; j++)
-				this.mutate(nemo[j]);
+		// gotta generate the first generation
+		for (var i=0; i < popSize; i++) {
+			this.population.push(generateOrganism());
 		}
-		nextGen = nextGen.concat(nemo); // will push all elements, if by chance it is an array because of multiple children.
 	}
-	nextGen = nextGen.slice(0, 20);
-	this.population = nextGen;
+	step(roundNum) {
+		this.simulate(this.population, roundNum);
+		var parents = this.successors(this.population);
+		var nextGen = [];
+		while (nextGen.length < this.popSize) {
+			// create two new organisms from two random parents.
+			var nemo = this.mate(parents[Math.floor(Math.random() * parents.length)], parents[Math.floor(Math.random() * parents.length)]);
+			if (typeof nemo.indexOf === "undefined")
+				this.mutate(nemo);
+			else {
+				for (var j=0; j < nemo.length; j++)
+					this.mutate(nemo[j]);
+			}
+			nextGen = nextGen.concat(nemo); // will push all elements, if by chance it is an array because of multiple children.
+		}
+		nextGen = nextGen.slice(0, 20);
+		this.population = nextGen;
+	}
+	successors(pop) {
+		pop = [].concat(pop);
+		var f = this.fitness;
+		pop.sort(function(a, b) {
+			return f(a) - f(b);
+		});
+		parents = [];
+		for (var i=0; i < this.survivalRate; i++)
+			parents.push(pop[i]);
+		return parents;
+	}
 }
-Darwin.prototype.successors = function(pop) {
-	pop = [].concat(pop);
-	var f = this.fitness;
-	pop.sort(function(a, b) {
-		return f(a) - f(b);
-	});
-	parents = [];
-	for (var i=0; i < this.survivalRate; i++)
-		parents.push(pop[i]);
-	return parents;
-}
+
 
 module.exports.Darwin = Darwin;
