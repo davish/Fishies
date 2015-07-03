91cee363dfc13541408924d50b11df4dab36a157
kevin <wangkevin448@gmail.com>
2015-07-03 15:09:03 -0700

    need to finish bounding box

--- js/fish.js
+++ js/fish.js
@@ -8,17 +8,24 @@ class Fish {
 	}
 
 	tick(time) {
-		//need to finish location bounds
-		// if(this.position.x > stateDimensions.x && this.velocity.t < Math.PI) {
-		//
-		// } else if(this.position.x < 0 && this.velocity.t >= Math.PI) {
-		//
-		// }
-		// if(this.position.y < 0 && (this.velocity.t < Math.PI/2 || this.velocity.t > Math.PI*3/2)) {
-		//
-		// } else if(this.position.y > stateDimensions.y && (this.velocity.t >= Math.PI/2 && this.velocity.t <= Math.PI*3/2)) {
-		//
-		// }
+		if(this.position.x > stateDimensions.x && this.velocity.t < Math.PI) {
+			this.position.x = stateDimensions.x;
+			this.velocity.t = Math.PI*2 - this.velocity.t;
+		} else if(this.position.x < 0 && this.velocity.t >= Math.PI) {
+			this.position.x = 0;
+			this.velocity.t = Math.PI*2 - this.velocity.t;
+		}
+		if(this.position.y < 0 && (this.velocity.t < Math.PI/2 || this.velocity.t > Math.PI*3/2)) {
+			this.velocity.t = Math.PI - this.velocity.t;
+			if(this.velocity.t < 0) {
+				this.velocity.t += Math.PI*2;
+			}
+		} else if(this.position.y > stateDimensions.y && (this.velocity.t >= Math.PI/2 && this.velocity.t <= Math.PI*3/2)) {
+			this.velocity.t = Math.PI - this.velocity.t;
+			if(this.velocity.t < 0) {
+				this.velocity.t += Math.PI*2;
+			}
+		}
 		this.position = {x: this.position.x + time/1000*this.velocity.r*Math.sin(this.velocity.t), y: this.position.y - time/1000*this.velocity.r*Math.cos(this.velocity.t)};
 	}
 }
