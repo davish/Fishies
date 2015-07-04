89b286f766182f408856f9d6cd2a70571dfbef36
fvoichick <fvoichick@gmail.com>
2015-07-03 23:16:35 -0700

    fixed the gaussian methods

--- js/state.js
+++ js/state.js
@@ -56,31 +56,34 @@ class State {
 //temporary
 class Algorithms {
   static randomWithinPercent(percent) {
-    let nextNextGaussian;
-    let haveNextNextGaussian = false;
-    let randomGaussian = () => {
-    	if (haveNextNextGaussian) {
-    		haveNextNextGaussian = false;
-    		return nextNextGaussian;
-    	} else {
-    		let v1, v2, s;
-    		do {
-    			v1 = 2 * Math.random() - 1;
-    			v2 = 2 * Math.random() - 1;
-    			s = v1*v1 + v2*v2;
-    		} while (s >= 1 || s == 0);
-    		let multiplier = Math.sqrt(-2 * Math.log(s)/s);
-    		nextNextGaussian = v2 * multiplier;
-    		haveNextNextGaussian = true;
-    		return v1 * multiplier;
-    	}
+    let gaussian = this.randomGaussian() * percent / 100;
+    if (gaussian >= 0) {
+      return 1 + gaussian;
+    } else {
+      return 1 / (1 - gaussian);
     }
-  	let gaussian = randomGaussian() * percent / 100;
-  	if (gaussian >= 0) {
-  		return 1 + gaussian;
-  	} else {
-  		return 1 / (1 + Math.abs(gaussian));
-  	}
+  }
+
+  static randomGaussian() {
+    if (!this.nextNextGaussian && this.nextNextGaussian != 0) {
+      this.nextNextGaussian = 0;
+      this.haveNextNextGaussian = false;
+    }
+    if (this.haveNextNextGaussian) {
+      this.haveNextNextGaussian = false;
+      return this.nextNextGaussian;
+    } else {
+      let v1, v2, s;
+      do {
+        v1 = 2 * Math.random() - 1;
+        v2 = 2 * Math.random() - 1;
+        s = v1*v1 + v2*v2;
+      } while (s >= 1 || s == 0);
+      let multiplier = Math.sqrt(-2 * Math.log(s)/s);
+      this.nextNextGaussian = v2 * multiplier;
+      this.haveNextNextGaussian = true;
+      return v1 * multiplier;
+    }
   }
 }
 
