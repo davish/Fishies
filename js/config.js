f03c5a5e32d85271a045dc302b7f4c01e6c25234
kevin <wangkevin448@gmail.com>
2015-07-07 12:45:44 -0700

    border on window

---
+++ js/config.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+  getProp(prop){
+    return this[prop];
+  },
+  dimensions: {
+    x: 800,
+    y: 600
+  }
+};
+
+module.exports = CONFIG;
--- js/fish.js
+++ js/fish.js
@@ -1,5 +1,8 @@
+let CONFIG = require('./config');
+let stateDimensions = CONFIG.dimensions;
+
 class Fish {
-	constructor(aChromosome, aPosition = {x: 0, y:0}, aVelocity = {r: 0, t:0}, stateDimensions) {
+	constructor(aChromosome, aPosition = {x: 0, y:0}, aVelocity = {r: 0, t:0}) {
 		this.chromosome = aChromosome;
 		this.velocity = aVelocity;
 		this.velocity = {r: 8, t: Math.random()*Math.PI*2};//temporary
@@ -9,24 +12,24 @@ class Fish {
 	}
 
 	tick(time) {
-		// if(this.position.x > stateDimensions.x && this.velocity.t < Math.PI) {
-		// 	this.position.x = stateDimensions.x;
-		// 	this.velocity.t = Math.PI*2 - this.velocity.t;
-		// } else if(this.position.x < 0 && this.velocity.t >= Math.PI) {
-		// 	this.position.x = 0;
-		// 	this.velocity.t = Math.PI*2 - this.velocity.t;
-		// }
-		// if(this.position.y < 0 && (this.velocity.t < Math.PI/2 || this.velocity.t > Math.PI*3/2)) {
-		// 	this.velocity.t = Math.PI - this.velocity.t;
-		// 	if(this.velocity.t < 0) {
-		// 		this.velocity.t += Math.PI*2;
-		// 	}
-		// } else if(this.position.y > stateDimensions.y && (this.velocity.t >= Math.PI/2 && this.velocity.t <= Math.PI*3/2)) {
-		// 	this.velocity.t = Math.PI - this.velocity.t;
-		// 	if(this.velocity.t < 0) {
-		// 		this.velocity.t += Math.PI*2;
-		// 	}
-		// }
+		if(this.position.x > stateDimensions.x && this.velocity.t < Math.PI) {
+			this.position.x = stateDimensions.x;
+			this.velocity.t = Math.PI*2 - this.velocity.t;
+		} else if(this.position.x < 0 && this.velocity.t >= Math.PI) {
+			this.position.x = 0;
+			this.velocity.t = Math.PI*2 - this.velocity.t;
+		}
+		if(this.position.y < 0 && (this.velocity.t < Math.PI/2 || this.velocity.t > Math.PI*3/2)) {
+			this.velocity.t = Math.PI - this.velocity.t;
+			if(this.velocity.t < 0) {
+				this.velocity.t += Math.PI*2;
+			}
+		} else if(this.position.y > stateDimensions.y && (this.velocity.t >= Math.PI/2 && this.velocity.t <= Math.PI*3/2)) {
+			this.velocity.t = Math.PI - this.velocity.t;
+			if(this.velocity.t < 0) {
+				this.velocity.t += Math.PI*2;
+			}
+		}
 
 		if (this.alive) {
 			this.position = {x: this.position.x + time/1000*this.velocity.r*Math.sin(this.velocity.t), y: this.position.y - time/1000*this.velocity.r*Math.cos(this.velocity.t)};
@@ -37,6 +40,15 @@ class Fish {
 
 	kill() {
 		this.alive = false;
+	}
+
+	static mate(momma, pappa) {
+		let split = Math.floor(Math.random() * momma.length);
+
+		let X = momma.chromosome.slice(0, split);
+		let Y = pappa.chromosome.slice(split);
+
+		return new Fish(X.concat(Y));
 	}
 }
 
