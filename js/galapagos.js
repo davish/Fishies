8bbdf74b3ecc9121cd3dc3342caa5834b083775a
Davis Haupt <dbh937@gmail.com>
2015-07-07 13:44:45 -0700

    began work on the synthesis of Darwin and the Simulation, on the Galapagos.

---
+++ js/galapagos.js
@@ -0,0 +1,8 @@
+let Darwin = require('../darwin').Darwin;
+let Chromosome = require('./chromosome');
+
+var calcFitness = function(F) {
+	return F.fitness;
+}
+
+let galapagos = new Darwin(Chromosome, )
