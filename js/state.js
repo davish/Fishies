f6a1064f4ff0a5b1003e653d4b3650f00ff577ae
Davis Haupt <dbh937@gmail.com>
2015-07-07 13:43:53 -0700

    moved algorithms to its own file

--- js/state.js
+++ js/state.js
@@ -2,9 +2,13 @@ let Food = require('./food');
 let Fish = require('./fish');
 let Chromosome = require('./chromosome');
 let CLOCK = require('./clock');
-
+let Algorithms = require('./algorithms');
 class State {
-  constructor() {
+  // population is an array containing Chromosomes.
+  // the chromosomes are objects containing the characteristics of a specific fish.
+  // they are the objects that are maniuplated by the genetic algorithm.
+  constructor(population) {
+
     this.fish = [];
     this.food = [];
     this.process = false;
@@ -13,9 +17,11 @@ class State {
     for (let i = 0; i < 10; i++) {
       this.food.push(new Food(10 * Algorithms.randomWithinPercent(25), {x: 800 * Math.random(), y: 600 * Math.random()}));
     }
-    for(let i = 0; i < 8; i++) {
-      this.fish.push(new Fish(new Chromosome(250*Algorithms.randomWithinPercent(variance), 2*Algorithms.randomWithinPercent(variance), 8*Algorithms.randomWithinPercent(variance), 256 * Math.random(), 256 * Math.random(), 256 * Math.random(), 256 * Math.random(), 256 * Math.random(), 256 * Math.random(), Math.PI / 2 * Algorithms.randomWithinPercent(50)), {x: 400*Algorithms.randomWithinPercent(25), y: 300*Algorithms.randomWithinPercent(25)}));
-    }
+	// how to generate a fish:
+	// 250*Algorithms.randomWithinPercent(variance), 2*Algorithms.randomWithinPercent(variance), 8*Algorithms.randomWithinPercent(variance), 256 * Math.random(), 256 * Math.random(), 256 * Math.random(), 256 * Math.random(), 256 * Math.random(), 256 * Math.random(), Math.PI / 2 * Algorithms.randomWithinPercent(50))
+	for (let i = 0; i < population.length; i++) {
+		this.fish.push(new Fish(population[i]));
+	}
   }
 
   addFish(aFish) {
@@ -53,44 +59,13 @@ class State {
 
   stop() {
     this.process = false;
+	for (let i = 0; i < this.fish.length; i++) {
+		this.fish[i].chromosome.fitness = this.fish[i].energy;
+	}
   }
 
   toString() {
 
-  }
-}
-
-//temporary
-class Algorithms {
-  static randomWithinPercent(percent) {
-    let gaussian = this.randomGaussian() * percent / 100;
-    if (gaussian >= 0) {
-      return 1 + gaussian;
-    } else {
-      return 1 / (1 - gaussian);
-    }
-  }
-
-  static randomGaussian() {
-    if (!this.nextNextGaussian && this.nextNextGaussian != 0) {
-      this.nextNextGaussian = 0;
-      this.haveNextNextGaussian = false;
-    }
-    if (this.haveNextNextGaussian) {
-      this.haveNextNextGaussian = false;
-      return this.nextNextGaussian;
-    } else {
-      let v1, v2, s;
-      do {
-        v1 = 2 * Math.random() - 1;
-        v2 = 2 * Math.random() - 1;
-        s = v1*v1 + v2*v2;
-      } while (s >= 1 || s == 0);
-      let multiplier = Math.sqrt(-2 * Math.log(s)/s);
-      this.nextNextGaussian = v2 * multiplier;
-      this.haveNextNextGaussian = true;
-      return v1 * multiplier;
-    }
   }
 }
 
