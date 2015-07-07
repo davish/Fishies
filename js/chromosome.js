74bc3eb744f9c3bc7f59ff9ad7879c6bac03aec5
Davis Haupt <dbh937@gmail.com>
2015-07-07 13:02:51 -0700

    fixed chromosome class

--- js/chromosome.js
+++ js/chromosome.js
@@ -1,19 +1,46 @@
 class Chromosome {
-  constructor(weight, lwRatio, tail, bodyR, bodyG, bodyB, eyeR, eyeG, eyeB, vision){
-    this.weight = weight;
-    this.lwRatio = lwRatio;
-    this.tail = tail;
-    this.bodyR = bodyR;
-    this.bodyG = bodyG;
-    this.bodyB = bodyB;
-    this.eyeR = eyeR;
-    this.eyeG = eyeG;
-    this.eyeB = eyeB;
-    this.vision = vision; //radians
+  constructor(weight, lwRatio, tail, bodyR, bodyG, bodyB, eyeR, eyeG, eyeB, vision, alpha, beta, gamma){
+	this.genes = [weight, lwRatio, tail, bodyR, bodyG, bodyB, eyeR, eyeG, eyeB, vision, alpha, beta, gamma];
   }
 
-  combine(chromoB) {
-
+  get weight(){
+	  return this.genes[0];
+  }
+  get lwRatio() {
+	  return this.genes[1];
+  }
+  get tail() {
+	  return this.genes[2];
+  }
+  get bodyR() {
+	  return this.genes[3];
+  }
+  get bodyG() {
+	  return this.genes[4];
+  }
+  get bodyB() {
+	  return this.genes[5];
+  }
+  get eyeR() {
+	  return this.genes[6];
+  }
+  get eyeG() {
+	  return this.genes[7];
+  }
+  get eyeB() {
+	  return this.genes[8];
+  }
+  get vision() {
+	  return this.genes[9];
+  }
+  get alpha() {
+	  return this.genes[10];
+  }
+  get beta() {
+	  return this.genes[11];
+  }
+  get gamma() {
+	  return this.genes[12];
   }
 }
 
