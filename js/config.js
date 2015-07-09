b6ff352c26ab8a8b330494a9da5579556d710364
fvoichick <fvoichick@gmail.com>
2015-07-08 21:16:28 -0700

    removed ghost fish

--- js/config.js
+++
@@ -1,8 +0,0 @@
-const CONFIG = {
-  dimensions: {
-    x: 800,
-    y: 600
-  }
-};
-
-module.exports = CONFIG;
--- js/food.js
+++
@@ -1,8 +0,0 @@
-class Food {
-  constructor(amount, position = {x: 0, y: 0}) {
-    this.amount = amount;
-    this.position = position;
-  }
-}
-
-module.exports = Food;
